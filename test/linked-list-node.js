f7e29b29e187bd744ef534943fab61897c2cbe7f
Steve Preston <steve.preston.80@gmail.com>
2018-03-15 22:17:14 -0700

    Made better test for next, factored out simple constructor test

--- test/linked-list-node.js
+++ test/linked-list-node.js
@@ -3,6 +3,20 @@ import ava from 'ava'
 const LinkedListNode = require('../lib/linked-list-node');
 
 ava('Linked List Node constructor', (t) => {
+    const n = new LinkedListNode(1);
+
+    t.is(n.data, 1, "Node 1's data is incorrect");
+    t.is(n.next, null, "Node 1's next is incorrect");
+});
+
+ava('Linked List Node default constructor', (t) => {
+    const n = new LinkedListNode();
+
+    t.is(n.data, undefined, "Node 1's data is incorrect");
+    t.is(n.next, null, "Node 1's next is incorrect");
+});
+
+ava('Linked List Node next', (t) => {
     const n1 = new LinkedListNode(1);
     const n2 = new LinkedListNode(2);
 
@@ -12,11 +26,6 @@ ava('Linked List Node constructor', (t) => {
     t.is(n2.data, 2, "Node 2's data is incorrect");
     t.is(n1.next, n2, "Node 1's next is incorrect");
     t.is(n2.next, null, "Node 2's next is incorrect");
-});
-
-ava('Linked List Node default constructor', (t) => {
-    const n = new LinkedListNode();
-
-    t.is(n.data, undefined, "Node 1's data is incorrect");
-    t.is(n.next, null, "Node 1's next is incorrect");
+    t.is(n1.next.data, 2, "Second node's data accessed via first node's next is incorrect");
+    t.is(n1.next.next, null, "Second node's next node accessed via first node's next is incorrect");
 });
