860a75a304ea01d042f8d0f4e849711bc6df2c2a
Steve Preston <steve.preston.80@gmail.com>
2018-03-15 22:13:01 -0700

    Added linked list node and tests

---
+++ test/linked-list-node.js
@@ -0,0 +1,22 @@
+import ava from 'ava'
+
+const LinkedListNode = require('../lib/linked-list-node');
+
+ava('Linked List Node constructor', (t) => {
+    const n1 = new LinkedListNode(1);
+    const n2 = new LinkedListNode(2);
+
+    n1.next = n2;
+
+    t.is(n1.data, 1, "Node 1's data is incorrect");
+    t.is(n2.data, 2, "Node 2's data is incorrect");
+    t.is(n1.next, n2, "Node 1's next is incorrect");
+    t.is(n2.next, null, "Node 2's next is incorrect");
+});
+
+ava('Linked List Node default constructor', (t) => {
+    const n = new LinkedListNode();
+
+    t.is(n.data, undefined, "Node 1's data is incorrect");
+    t.is(n.next, null, "Node 1's next is incorrect");
+});
