35ed13b9322b1d9b5df2d8e22c2c3a958dedb8b7
Steve Preston <steve.preston.80@gmail.com>
2018-05-17 22:18:08 -0700

    Added stack and stack tests

---
+++ test/stack.js
@@ -0,0 +1,84 @@
+import ava from 'ava';
+const Stack = require('../lib/stack');
+
+ava('Stack constructor', (t) => {
+    const s = new Stack();
+
+    t.is(s.size, 0, 'The default stack size is incorrect');
+    t.deepEqual(s.collection, [], 'The default collection is incorrect');
+});
+
+ava('Stack constructor with good param (array)', (t) => {
+    const data = ['test', 'test2'];
+    const s = new Stack(data);
+
+    t.is(s.size, 2, 'The stack size is incorrect');
+    t.deepEqual(s.collection, data, 'The collection is incorrect');
+});
+
+ava('Stack constructor with good param (null)', (t) => {
+    const data = null;
+    const s = new Stack(data);
+
+    t.is(s.size, 0, 'The stack size is incorrect');
+    t.deepEqual(s.collection, [], 'The collection is incorrect');
+});
+
+ava('Stack constructor with bad param (number)', (t) => {
+    const data = 0;
+
+    t.throws(() => {
+        const s = new Stack(data);
+    }, TypeError);
+});
+
+ava('Stack constructor with bad param (object)', (t) => {
+    const data = {};
+
+    t.throws(() => {
+        const s = new Stack(data);
+    }, TypeError);
+});
+
+ava('Stack push', (t) => {
+    const data = ['test', 'test2', 'test3'];
+
+    const s = new Stack();
+
+    data.forEach((element) => {
+        s.push(element);
+    });
+
+    t.is(s.size, 3, 'The stack size is incorrect');
+    t.deepEqual(s.collection, data, 'The stack size is incorrect');
+});
+
+ava('Stack push', (t) => {
+    const data = ['test', 'test2', 'test3'];
+
+    const s = new Stack();
+    let ret;
+
+    data.forEach((element) => {
+        ret = s.push(element);
+    });
+
+    t.is(s.size, 3, 'The stack size is incorrect');
+    t.deepEqual(s.collection, data, 'The stack size is incorrect');
+    t.is(ret, undefined, 'Stack.push should not return a value');
+});
+
+ava('Stack pop', (t) => {
+    const data = ['test', 'test2', 'test3'];
+
+    const s = new Stack(data);
+
+    t.is(s.size, 3, 'The stack size is incorrect');
+    t.deepEqual(s.collection, data, 'The stack size is incorrect');
+
+    const element = s.pop();
+
+    t.is(s.size, 2, 'The stack size is incorrect');
+    t.is(element, data[2], 'The popped element is incorrect');
+    t.deepEqual(s.collection, [data[0], data[1]], 'The stack state is incorrect');
+});
